59db455e6414d257da8783b7483a57f820f8415f
Kirill Baranovskiy <kirillbaranovskiy01@gmail.com>
2025-05-12 20:02:54 +0300

    add changes in main

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log('new info');
 
 console.log('develop');
 alert('new');
+
+const userAge = 35;
+if (userAge >= 18) {
+  console.log('Вы совершенолетний');
+} else {
+  console.log('Вы Не совершенолетний');
+}
