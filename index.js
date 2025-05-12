6b9903770b563203bee442ae90e37a1c4d38ce53
Kirill Baranovskiy <kirillbaranovskiy01@gmail.com>
2025-05-12 19:58:27 +0300

    made changes in feature/user-access

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ console.log('new info');
 
 console.log('develop');
 alert('new');
+
+const age = 20;
+if (age >= 18) {
+  console.log('Вы совершенолетний');
+} else {
+  console.log('Вы НЕ совершенолетний');
+}
